049fa2dbcdf0489b31d960b8c9a015f30bf9859b
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-04-30 13:11:04 +0600

    added cors origin

--- index.js
+++ index.js
@@ -7,15 +7,11 @@ const app=express()
 
 
 
-
 //middleware
-// app.use(cors())
-
 app.use(cors({
-  origin: ['http://localhost:5173/', 'https://incomparable-pudding-212c9c.netlify.app/'],
-  methods: 'GET, POST, DELETE, PUT',
-  credentials: true,
-}));
+  origin: "http://localhost:5173",
+  methods:["GET", "POST", "PUT", "PATCH", "DELETE","OPTIONS"]
+}))
 app.use(express.json())
 
 const port=process.env.PORT||5000;
@@ -34,7 +30,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const employeeCollection=client.db("employeeDB").collection("employee")
 
@@ -81,8 +77,8 @@ async function run() {
         });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
