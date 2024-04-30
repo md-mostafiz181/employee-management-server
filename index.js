b7493ad1d9415c03d5fe603c815c083ed3f61705
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-04-30 10:13:40 +0600

    remove unnecessery code

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors(
   }
 ))
 
-app.use((req, res, next) => { res.header({"Access-Control-Allow-Origin": "*"}); next(); })
+
 app.use(express.json())
 
 const port=process.env.PORT||5000;
